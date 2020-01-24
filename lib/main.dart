83f950c9bb04ed3fdd41536b342fcd0eb728e32b
Cold Stone <xiaoranran1993@outlook.com>
2020-01-25 07:13:35 +0800

    feat: add google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -39,6 +40,7 @@ class MyApp extends StatelessWidget {
               title: 'Flin',
               theme: ThemeData(
                 primarySwatch: theme.color,
+                textTheme: GoogleFonts.robotoMonoTextTheme(),
               ),
               localizationsDelegates: [
                 GlobalMaterialLocalizations.delegate,
