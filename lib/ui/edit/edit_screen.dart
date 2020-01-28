ce727faebc7c76476c84fa0fcd977fecdfe7cf7d
Cold Stone <xiaoranran1993@outlook.com>
2020-01-28 23:32:54 +0800

    feat: story rate

--- lib/ui/edit/edit_screen.dart
+++ lib/ui/edit/edit_screen.dart
@@ -25,37 +25,23 @@ class _EditScreenState extends State<EditScreen> {
   SwiperController _swiperController = SwiperController();
   String _watchDate = _formatDate(DateTime.now());
   int _feelIndex = 0;
+  double _rate = 5.0;
 
   static String _formatDate(DateTime date) {
     return formatDate(date, [yyyy, '.', mm, '.', dd]);
   }
 
-  FlatButton _moveTo(int index) {
-    return FlatButton(
-      child: Text('next', style: Styles.info),
-      onPressed: () {
-        _swiperController.move(index);
-      },
-    );
-  }
-
-  Widget _buildWatchDate(String poster) {
+  Widget _buildWatchDate() {
     return Column(
       children: <Widget>[
-        SizedBox(height: 24.0),
-        ExtendedImage.network(
-          IMG_PREFIX + poster,
-          cache: true,
-          height: 200.0,
-          fit: BoxFit.cover,
-          shape: BoxShape.rectangle,
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-        ),
-        SizedBox(height: 24.0),
+        SizedBox(height: 48.0),
         Text(StoryQuestion.watchDate, style: Styles.normal),
-        SizedBox(height: 140.0),
-        Text(_watchDate.toString(), style: Styles.title),
-        SizedBox(height: 24.0),
+        SizedBox(height: 100.0),
+        Text(
+          _watchDate.toString(),
+          style: Styles.title.copyWith(fontSize: 36.0),
+        ),
+        SizedBox(height: 64.0),
         FlatButton(
           padding: EdgeInsets.symmetric(horizontal: 20.0),
           child: Text(
@@ -71,47 +57,32 @@ class _EditScreenState extends State<EditScreen> {
               initialDatePickerMode: DatePickerMode.year,
             );
             setState(() {
-              _watchDate = _formatDate(selectedDate);
-              _swiperController.next();
+              if (selectedDate != null) {
+                _watchDate = _formatDate(selectedDate);
+                _swiperController.next();
+              }
             });
           },
         ),
-        Spacer(),
-        _moveTo(1),
       ],
     );
   }
 
-  Widget _buildFeeling(String poster) {
-    return Stack(
+  Widget _buildFeeling() {
+    return Column(
       children: <Widget>[
-        Positioned(
-          top: 24.0,
-          left: 24.0,
-          child: ExtendedImage.network(
-            IMG_PREFIX + poster,
-            cache: true,
-            width: 100.0,
-            height: 100.0,
-            fit: BoxFit.cover,
-            shape: BoxShape.circle,
-          ),
-        ),
-        Positioned(
-          top: 148.0,
-          left: 24.0,
-          child: Text(StoryQuestion.feeling, style: Styles.normal),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: SizedBox(
-            width: double.infinity,
-            height: 124.0,
+        SizedBox(height: 48.0),
+        Text(StoryQuestion.feeling, style: Styles.normal),
+        SizedBox(height: 80.0),
+        SizedBox(
+          width: double.infinity,
+          height: 124.0,
+          child: Scrollbar(
             child: ListView.builder(
               itemExtent: 124.0,
               scrollDirection: Axis.horizontal,
               itemCount: FeelEmoji.list.length,
-              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              padding: EdgeInsets.symmetric(horizontal: 48.0),
               itemBuilder: (context, index) {
                 final asset = FeelEmoji.list[index];
                 final svg = asset['svg'];
@@ -147,24 +118,54 @@ class _EditScreenState extends State<EditScreen> {
             ),
           ),
         ),
-        Positioned(
-          top: 500.0,
-          left: screenWidth(context) / 2 - 60,
-          child: FlatButton(
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
-            onPressed: () {
-              _swiperController.next();
-            },
-            child: Text(
-              'Confirm',
-              style:
-                  Styles.normal.copyWith(decoration: TextDecoration.underline),
-            ),
+        SizedBox(height: 42.0),
+        FlatButton(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          onPressed: () {
+            _swiperController.next();
+          },
+          child: Text(
+            'Confirm',
+            style: Styles.normal.copyWith(decoration: TextDecoration.underline),
           ),
         ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: _moveTo(2),
+      ],
+    );
+  }
+
+  Widget _buildRate() {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 48.0),
+        Text(StoryQuestion.feeling, style: Styles.normal),
+        SizedBox(height: 60.0),
+        Text(
+          _rate.toStringAsFixed(1),
+          style: Styles.title.copyWith(fontSize: 36.0),
+        ),
+        SizedBox(height: 24.0),
+        Slider(
+          min: 0.0,
+          max: 10.0,
+          value: _rate,
+          activeColor: Colors.white,
+          inactiveColor: Colors.white30,
+          onChanged: (double value) {
+            setState(() {
+              _rate = value;
+            });
+          },
+        ),
+        SizedBox(height: 42.0),
+        FlatButton(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          onPressed: () {
+            _swiperController.next();
+          },
+          child: Text(
+            'Confirm',
+            style: Styles.normal.copyWith(decoration: TextDecoration.underline),
+          ),
         ),
       ],
     );
@@ -174,9 +175,11 @@ class _EditScreenState extends State<EditScreen> {
   Widget build(BuildContext context) {
     final Movie _movie = widget.movie;
     String poster = _movie.posterPath ?? _movie.backdropPath;
+
     List pages = [
-      _buildWatchDate(poster),
-      _buildFeeling(poster),
+      _buildWatchDate(),
+      _buildFeeling(),
+      _buildRate(),
     ];
 
     return DecoratedBox(
@@ -185,43 +188,34 @@ class _EditScreenState extends State<EditScreen> {
         child: Scaffold(
           appBar: AppBar(elevation: 0.0, backgroundColor: Colors.transparent),
           backgroundColor: Colors.transparent,
-          body: Swiper(
-            loop: false,
-            itemCount: pages.length,
-            controller: _swiperController,
-            // control: SwiperControl(),
-            // pagination: SwiperPagination(
-            //   margin: EdgeInsets.all(12.0),
-            //   alignment: Alignment.topRight,
-            // ),
-            pagination: SwiperCustomPagination(
-              builder: (BuildContext context, SwiperPluginConfig config) {
-                int activeIndex = config.activeIndex;
-                return Positioned(
-                  top: 24.0,
-                  right: 24.0,
-                  child: Column(
-                    children: List.generate(config.itemCount, (index) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: activeIndex == index
-                              ? Colors.white
-                              : Colors.white54,
-                        ),
-                        margin: EdgeInsets.only(bottom: 6.0),
-                        width: 8.0,
-                        height: 8.0,
-                      );
-                    }),
+          body: Column(
+            children: <Widget>[
+              SizedBox(height: 24.0),
+              ExtendedImage.network(
+                IMG_PREFIX + poster,
+                cache: true,
+                height: 200.0,
+                fit: BoxFit.cover,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              SizedBox(height: 24.0),
+              Expanded(
+                child: Swiper(
+                  loop: false,
+                  itemCount: pages.length,
+                  controller: _swiperController,
+                  scrollDirection: Axis.vertical,
+                  control: SwiperControl(
+                    size: 24.0,
+                    color: Colors.white,
                   ),
-                );
-              },
-            ),
-            scrollDirection: Axis.vertical,
-            itemBuilder: (BuildContext context, int index) {
-              return pages[index];
-            },
+                  itemBuilder: (BuildContext context, int index) {
+                    return pages[index];
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
