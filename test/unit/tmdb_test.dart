30e0f3cae60b1c317ba0dc3fb041b401d135be84
Cold Stone <xiaoranran1993@outlook.com>
2020-02-09 14:52:28 +0800

    ci: update

--- test/unit/tmdb_test.dart
+++ test/unit/tmdb_test.dart
@@ -1,13 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import '../../lib/model/movie.dart';
-import '../../lib/data/network/tmdb.dart';
+// import '../../lib/model/movie.dart';
+// import '../../lib/data/network/tmdb.dart';
 
 main() async {
   group('Tmdb tests', () {
-    final query = {
-      "page": 1,
-    };
+    // final query = {
+    //   "page": 1,
+    // };
 
     test('Fetch movies successfully', () async {
       // List<Movie> movies = await Tmdb().getMovies(query: query);
